5146d4458509fa9e23d21c763f01afa1bd25b170
kavyasri2 <peechara3@gmail.com>
2016-02-06 19:18:22 -0800

    Initial commit

---
+++ python1/__init__.py
@@ -0,0 +1,14 @@
+import random
+import sys
+import os
+
+print("Hello kavya")
+name="Sweety"
+print(name)
+name = 15
+print("5+2=",5+2)
+print("5-2=",5-2)
+print("5*2=",5*2)
+print("5/2=",5/2)
+print("5%2=",5%2)
+print ("Hello, Python!")
